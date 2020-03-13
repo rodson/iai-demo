0ce86457da43a5a78fe16676dd2b6514fa6534e8
rodsonluo <rodsonluo@tencent.com>
2020-03-13 22:34:59 +0800

    init

--- app.js
+++ app.js
@@ -5,19 +5,3 @@ const bodyParser = require('koa-bodyparser');
 const detectFace = require('./apis/detectFace');
 const static = require('koa-static')
 const path = require('path');
-
-const app = new Koa();
-app.use(bodyParser());
-
-// 设置静态资源目录
-app.use(static(path.join( __dirname,  './static')));
-
-// 接口
-const router = new Router();
-router.post('/detectFace', detectFace);
-app.use(router.routes());
-
-app.listen(3000);
-
-module.exports = app;
-
--- iaiClient.js
+++ iaiClient.js
@@ -1,4 +1,3 @@
-
 // 密钥文件
 const properties = require('./properties.json');
 // 引入腾讯云SDK
@@ -9,32 +8,3 @@ const IaiClient = tencentcloud.iai.v20180301.Client;
 const models = tencentcloud.iai.v20180301.Models;
 // 身份凭证类，存储secretId和secretKey
 const Credential = tencentcloud.common.Credential
-
-// 实例化调用client
-let cred = new Credential(properties.secretId, properties.secretKey);
-let client = new IaiClient(cred);
-
-function detectFace(image) {
-  return new Promise((resolve, reject) => {
-
-    // 请求参数
-    const req = new models.DetectFaceRequest();
-    req.Image = image;
-    req.FaceModelVersion = '3.0';
-    req.NeedFaceAttributes = 1;
-    req.NeedQualityDetection = 1;
-
-    // 发送请求
-    client.DetectFace(req, (errMsg, response) => {
-        if (errMsg) {
-          reject(errMsg);
-          return;
-        }
-        resolve(response);
-    });
-  });
-}
-
-module.exports = {
-  detectFace
-};

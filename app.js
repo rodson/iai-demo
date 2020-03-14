d6b396bcf32ca358be8fd26c568c89c970ffe961
rodsonluo <rodsonluo@tencent.com>
2020-03-14 10:54:23 +0800

    update

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-  // 密钥文件
+// 密钥文件
 const properties = require('./properties.json');
 
 // 引入腾讯云SDK
 const tencentcloud = require('tencentcloud-sdk-nodejs');
+
 // 人脸识别client类，封装了人脸识别接口的调用
-const Client = tencentcloud.iai.v20180301.Client;
 // 人脸识别Model，封装了各接口的请求结构
-const Models = tencentcloud.iai.v20180301.Models;
+const { Client, Models } = tencentcloud.iai.v20180301;
+
 // 身份凭证类，存储secretId和secretKey
 const Credential = tencentcloud.common.Credential;
 
